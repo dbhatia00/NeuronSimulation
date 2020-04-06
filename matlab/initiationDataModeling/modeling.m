7c9c2234ac4b525140704329a3652533b116022c
Dev B <devb@bu.edu>
2020-04-06 19:09:15 -0400

    Graphs

--- matlab/initiationDataModeling/modeling.m
+++ matlab/initiationDataModeling/modeling.m
@@ -1,7 +1,13 @@
 clear
 fID = fopen('030220Patch2_temp_14_smth.txt','r');
+x = 1:199999;
+x = x';
 sizeA = [1 inf];
 formatSpec = '%f';
 y = fscanf(fID, formatSpec, sizeA);
 y = y';
-plot(1:199999, y);
+plot(x, y);
+title('Potential vs. Time')
+xlabel('Time') 
+ylabel('Membrane Potential mV') 
+clear fID formatSpec sizeA
