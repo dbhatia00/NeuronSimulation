5005677a6ecc7dd500824e6e381897fc853be0a5
Mizzou Neuro <devb@bu.edu>
2020-06-23 13:08:24 -0500

    new variables

--- python/tempEQ.py
+++ python/tempEQ.py
@@ -7,14 +7,14 @@ def integrand(x, tempC, lilR, bigR):
 
 t = 500 *pow(10,-3) #s
 wavelength = 1994 * pow(10,-9) #m
-ua = 50 *100  #m^-1, optical absorbtion coeff
+ua = 73.9 *100  #m^-1, optical absorbtion coeff
 k = 0.6 #W m^-1 K^-1
 p = 1000 #kg/m^-3
-c = 4188 # J kg^-1 K^-1
+c = 4184 # J kg^-1 K^-1
 pi = 3.14
 P0 = [0 * pow(10,-3),5 * pow(10,-3),10 * pow(10,-3), 15 * pow(10,-3),20 * pow(10,-3)] #W // Array of power values
-R = 93.41 * pow(10,-6)#m
-z = 164.56 * pow(10,-6)#m
+R = 25.51* pow(10,-6)#m
+z = 117.55 * pow(10,-6)#m
 r = 25 * pow(10,-6) #m
 dt = []
 
@@ -26,6 +26,8 @@ for powers in range(0,len(P0)):
 	insideIntegral = integrate.quad(integrand, 0, t, args = (tc,r, R))
 	dt.append(insideIntegral[0]*constantBeforeIntegral)
 	print("deltaT = ", insideIntegral[0]*constantBeforeIntegral, "C when P = " , P0[powers] , "W")
+
+print('Thermal time constant ' , tc)
 
 plt.plot(P0,dt)
 plt.xlabel('Laser Power (W)')
